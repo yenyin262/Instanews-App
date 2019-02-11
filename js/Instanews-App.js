30a02ad1bdd5255aae0a9fbd3843dd255db0c775
Yen Yin Hing <yenyinf287@gmail.com>
2019-02-10 16:37:57 -0800

    delete comments and code clean fix

--- js/Instanews-App.js
+++ js/Instanews-App.js
@@ -19,9 +19,9 @@ $(function () {
 
         $('.loaderGif').hide();
         let counter = 0;
-        // $.each(news, function (key, value) {
+
         $.each(news, (key, value) => {
-            
+
             if (value.multimedia.length === 5 && counter < 12) {
                 counter = counter + 1;
                 const imageUrl = value.multimedia[4].url;
@@ -38,17 +38,17 @@ $(function () {
             'api-key': 'VgfxUztDMZve2b4mN3eRWPXLVz35FvgT'
         });
         let url = apiUrl + event.target.value + '.json' + apiKey;
-        
+
         $.ajax({
             url: url,
             method: 'GET',
-            
+
         }).done((data) => fetchArticles(data))
-        .fail(function (err) {
-        throw err;
-        });
+            .fail(function (err) {
+                throw err;
+            });
     });
-    
+
     function tabletCSS() {
         main.css({
             'height': '16vh',
@@ -57,20 +57,20 @@ $(function () {
             'justify-content': 'center',
             'align-items': 'center',
         })
-        
+
         logoContainer.css({
             'height': '100px',
             'margin-top': '0px',
         })
-        
+
         logo.css({
             'height': '65px',
         })
-        
+
         description.css({
             'margin-left': '-1px',
         })
-        
+
         dropDownContainer.css({
             'margin-top': '0px',
             'align-items': 'unset',
@@ -78,32 +78,32 @@ $(function () {
             'margin-left': '90px',
             'margin-right': '40px',
         })
-        
+
         selectDropDownContainer.css({
             'margin-left': '35px',
             'align-self': 'flex-start',
         })
-        
+
         ajax.css({
             'margin-top': '20px',
         });
-        
+
         footerCopyright.css({
             'margin-top': '20px',
         })
     }
-    
+
     function desktopCSS() {
         main.css({
             'height': '15vh',
             'padding-top': '10px',
             'padding-bottom': '10px',
         })
-        
+
         logo.css({
             'height': '65px',
         })
-        
+
         logoContainer.css({
             'margin-left': '50px',
             'margin-right': '80px',
@@ -111,65 +111,65 @@ $(function () {
         dropDownContainer.css({
             'margin-top': '-6px'
         })
-        
+
         selectDropDownContainer.css({
             'margin-left': '38px',
         })
-        
+
         ajax.css({
             'margin-top': '5%',
             'margin-left': '-64%',
         })
-        
+
         footerCopyright.css({
             'margin-top': '20px',
         })
-        
+
     }
-    
-    function mobileCSS() { 
-        
+
+    function mobileCSS() {
+
         main.css({
             'height': '55vh',
             'padding-bottom': '20px',
             'padding-top': '20px',
         })
-        
+
         logoContainer.css({
             'margin-top': '0px',
             'height': '40vh',
         })
-        
+
         dropDownContainer.css({
             'margin-top': '10px',
         })
-        
+
         ajax.css({
             'margin-left': '180px',
         })
-        
+
         footerCopyright.css({
             'margin-top': '20px',
         })
-        
+
     }
-   
+
     selectOption.on('change', () => {
         ajax.css({
             'display': 'inline',
         });
-        
+
         selectOption.find('option[value=\'section\']').remove();
-        // function tabletCSS()
+
         if ($(window).width() >= 600 && $(window).width() < 1240) {
             tabletCSS();
         } else if ($(window).width() >= 1240) {
             desktopCSS();
         } else {
             mobileCSS();
-            
+
         }
-        
+
     });
-    
+
 });
